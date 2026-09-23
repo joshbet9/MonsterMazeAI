d3ce4e1b5affc55e3d4f12ffa845a69809ef7519
joshbet9 <josh.bettman123@gmail.com>
2026-09-23 19:08:47 +1000

    Expose kit abilities to planner action space

--- common/src/main/java/me/monstermazeai/planner/ActionSpace.java
+++ common/src/main/java/me/monstermazeai/planner/ActionSpace.java
@@ -1,11 +1,44 @@
 package me.monstermazeai.planner;
 
+import me.monstermazeai.game.GameState;
+import me.monstermazeai.kit.Kit;
 import me.monstermazeai.player.Action;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public final class ActionSpace {
     private ActionSpace() {}
+
+    public static List<Action> actions(GameState game, boolean allowJump) {
+        ArrayList<Action> out = new ArrayList<>(movementActions(allowJump));
+
+        // Ability activation is deliberately an action dimension. This lets
+        // the search discover emergency use rather than requiring a scripted
+        // "use ability when close" rule.
+        if (canUseAbility(game)) {
+            ArrayList<Action> withAbility = new ArrayList<>(out.size());
+            for (Action a : out) {
+                withAbility.add(new Action(a.forward(), a.strafe(), a.jump(),
+                        a.sprint(), a.yawDelta(), true));
+            }
+            out.addAll(withAbility);
+        }
+        return out;
+    }
+
+    private static boolean canUseAbility(GameState game) {
+        return switch (game.kit) {
+            case JUMPER -> false;
+            case SLOWBALLER -> game.mode != me.monstermazeai.game.Mode.ORIGINAL
+                    && game.tick >= game.ability.cooldownUntilTick;
+            case BODY_BUILDER -> game.mode != me.monstermazeai.game.Mode.ORIGINAL
+                    && game.ability.activations > 0
+                    && game.ability.activeUntilTick <= game.tick;
+            case REPULSOR -> game.ability.charges > 0;
+            case MAVERICK -> false;
+        };
+    }
 
     public static List<Action> movementActions(boolean allowJump) {
         return movementActions(allowJump, true);

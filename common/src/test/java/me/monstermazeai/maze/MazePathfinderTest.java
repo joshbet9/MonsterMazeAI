ed8032034083b03b2aba2bcea869757ea2eba9af
joshbet9 <josh.bettman123@gmail.com>
2026-09-24 21:42:26 +1000

    Test Safe Pad over air remains physically reachable

--- common/src/test/java/me/monstermazeai/maze/MazePathfinderTest.java
+++ common/src/test/java/me/monstermazeai/maze/MazePathfinderTest.java
@@ -58,6 +58,20 @@ class MazePathfinderTest {
     }
 
     @Test
+    void playerCanReachSafePadWhenUnderlyingLayoutIsAir() {
+        int[][] raw = new int[MazeModel.SIZE][MazeModel.SIZE];
+        raw[49][48] = 1;
+        raw[49][49] = 0;
+        MazeModel maze = new MazeModel(raw);
+        maze.setDisabled(49, 49, true);
+
+        List<Cell> path = new PlayerPathfinder().shortestPath(
+                maze, new Cell(49, 48), new Cell(49, 49));
+
+        assertEquals(List.of(new Cell(49, 48), new Cell(49, 49)), path);
+    }
+
+    @Test
     void playerCanReachDisabledSafePadFloor() {
         MazeModel maze = openMaze();
         for (int r = 47; r <= 51; r++) {

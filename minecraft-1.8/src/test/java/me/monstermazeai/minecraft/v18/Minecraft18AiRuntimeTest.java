fece7b84ae7c73ef1066a2f6cd1206c0a8c6c149
joshbet9 <josh.bettman123@gmail.com>
2026-09-25 11:33:38 +1000

    test: make Minecraft runtime configuration deterministic
    Make runtime configuration resolution testable without depending on developer environment variables. Preserve property/environment precedence and add coverage for Java executable resolution.

--- minecraft-1.8/src/test/java/me/monstermazeai/minecraft/v18/Minecraft18AiRuntimeTest.java
+++ minecraft-1.8/src/test/java/me/monstermazeai/minecraft/v18/Minecraft18AiRuntimeTest.java
@@ -6,14 +6,51 @@ import static org.junit.Assert.*;
 
 public class Minecraft18AiRuntimeTest {
     @Test
-    public void runtimeIsOptInWhenNoJarIsConfigured() {
-        String previous = System.getProperty("monstermazeai.runtime.jar");
-        try {
-            System.clearProperty("monstermazeai.runtime.jar");
-            Minecraft18AiRuntime runtime = new Minecraft18AiRuntime();
-            assertFalse(runtime.configured());
-        } finally {
-            if (previous != null) System.setProperty("monstermazeai.runtime.jar", previous);
-        }
+    public void runtimeConfigurationIsOptInWhenNoJarIsConfigured() {
+        assertNull(Minecraft18AiRuntime.resolveRuntimeJar(null, null));
+        assertNull(Minecraft18AiRuntime.resolveRuntimeJar("  ", "\t"));
+    }
+
+    @Test
+    public void runtimeJarPropertyOverridesEnvironment() {
+        assertEquals("property.jar",
+                Minecraft18AiRuntime.resolveRuntimeJar("  property.jar  ", "environment.jar"));
+    }
+
+    @Test
+    public void runtimeJarFallsBackToEnvironment() {
+        assertEquals("environment.jar",
+                Minecraft18AiRuntime.resolveRuntimeJar(null, "  environment.jar  "));
+    }
+
+    @Test
+    public void javaPropertyOverridesEnvironmentAndJavaHome() {
+        assertEquals("C:\\custom\\java.exe",
+                Minecraft18AiRuntime.resolveJavaExecutable(
+                        "  C:\\custom\\java.exe  ",
+                        "C:\\environment\\java.exe",
+                        "C:\\java-home"));
+    }
+
+    @Test
+    public void javaEnvironmentOverridesJavaHome() {
+        assertEquals("C:\\environment\\java.exe",
+                Minecraft18AiRuntime.resolveJavaExecutable(
+                        null,
+                        "  C:\\environment\\java.exe  ",
+                        "C:\\java-home"));
+    }
+
+    @Test
+    public void javaHomeBuildsJavaExecutable() {
+        assertEquals("C:\\java-home\\bin\\java.exe",
+                Minecraft18AiRuntime.resolveJavaExecutable(null, null, "C:\\java-home"));
+        assertEquals("C:\\java-home\\bin\\java.exe",
+                Minecraft18AiRuntime.resolveJavaExecutable(null, null, "C:\\java-home\\"));
+    }
+
+    @Test
+    public void javaFallsBackToPathLookup() {
+        assertEquals("java", Minecraft18AiRuntime.resolveJavaExecutable(null, null, null));
     }
 }
